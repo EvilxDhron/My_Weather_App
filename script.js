01e2d5390b64f6e4c001a042868028700ef58656
EvilxDhron <dhronyadavdhronyadav@gmail.com>
2025-07-12 12:17:47 +0530

    Modified forecastItems

--- script.js
+++ script.js
@@ -13,6 +13,10 @@ const conditionText = document.querySelector(".condition-text");
 const humidityValue = document.querySelector(".humidity-value-text");
 const windValue = document.querySelector(".wind-value-text");
 
+const forecastItemscontainer = document.querySelector(
+  ".forecast-items-container"
+);
+
 const apiKey = "bbe61a4c8d7f44c73200b9df8f3d1755";
 
 searchBtn.addEventListener("click", () => {
@@ -58,6 +62,17 @@ function getWeatherImg(id) {
   return img;
 }
 
+function getCurrentDate() {
+  const currentDate = new Date();
+  const dateObj = {
+    weekday: "short",
+    day: "2-digit",
+    month: "short",
+  };
+
+  return currentDate.toLocaleDateString("en-GB", dateObj);
+}
+
 async function updateWeatherInfo(city) {
   const weatherData = await getFetchData("weather", city);
 
@@ -81,8 +96,63 @@ async function updateWeatherInfo(city) {
   windValue.textContent = speed + " M/s";
 
   weatherSummaryImg.src = `/Weather_App/Assets/${getWeatherImg(id)}`;
+  currentDate.textContent = getCurrentDate();
 
+  await updateForecastInfo(city);
   console.log(weatherData);
+}
+
+async function updateForecastInfo(city) {
+  const forecastData = await getFetchData("forecast", city);
+
+  const timeTaken = "12:00:00";
+  const todayDate = new Date().toISOString().split("T")[0];
+
+  forecastItemscontainer.innerHTML = '';
+  forecastData.list.forEach((forecastWeather) => {
+    if (
+      forecastWeather.dt_txt.includes(timeTaken) &&
+      !forecastWeather.dt_txt.includes(todayDate)
+    ) {
+      // console.log(forecastWeather);
+      updateForecastItems(forecastWeather);
+    }
+  });
+  console.log(todayDate);
+}
+
+function updateForecastItems(weatherData) {
+  console.log(weatherData);
+
+  const {
+    dt_txt: date,
+    weather: [{ id }],
+    main: { temp },
+  } = weatherData;
+
+  console.log(new Date(date));
+  const dateTaken = new Date(date);
+  const dateOptions = {
+    day: '2-digit',
+    month: 'short'
+  }
+
+  const dateResult = dateTaken.toLocaleDateString("en-GB", dateOptions);
+
+
+  const forecastItem = `
+      <div class="forecast-item">
+        <h5 class="forecast-item-date">${dateResult}</h5>
+          <img
+            src="/Weather_App/Assets/${getWeatherImg(id)}"
+            alt=""
+            class="forecast-img"
+          />
+        <h5 class="forecast-item-temp">${Math.round(temp)} °C</h5>
+      </div>  
+  `;
+
+  forecastItemscontainer.insertAdjacentHTML('beforeend', forecastItem);
 }
 
 function showDisplaySection(section) {
